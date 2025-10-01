e71747d6f8074b59bbf445d3c71b9a5bc52a3097
Habib <habibtrmojo@gmail.com>
2025-10-01 11:31:15 +0700

    Add/Update nextapp/app/loading.tsx

---
+++ nextapp/app/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="flex min-h-[50vh] items-center justify-center">
+      <div className="h-10 w-10 animate-spin rounded-full border-4 border-white/20 border-t-fuchsia-400" />
+    </div>
+  );
+}
